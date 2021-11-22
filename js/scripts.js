baa48314ea3baaaf6192e1ee11e631d36c96aaa5
Glenn Langdon <glennlangdon@gmail.com>
2021-11-22 14:50:36 -0600

    navbar

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,29 @@
 window.addEventListener("DOMContentLoaded", function () {
 
+    let navbar = document.querySelector(".nav-wrapper");
+    // return original scrollY value
+    let scroll = window.scrollY;
+    let navHeight = navbar.clientHeight;
+    
+    window.addEventListener("scroll", function () {
+        let scrolled = window.scrollY;
+        // animate navbar off screen when scrollY exceeds navbar height
+        if (scrolled > navHeight) {
+            navbar.classList.add("animate");
+        } else {
+            navbar.classList.remove("animate");
+        }
+        // when y offset is greater than original scroll position
+        // remove sticky navbar
+        if (scrolled > scroll) {
+            navbar.classList.remove("sticky");
+        } else {
+            navbar.classList.add("sticky");
+        }
+        // on each scroll event reset original scroll position to current y offset
+        scroll = scrolled;
+    });
+  
+
     console.log("DOM Content loaded and parsed")
 });
